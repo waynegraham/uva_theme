1ac6c7a40979093fd17ab715b071006d27073d4b
Wayne Graham <wayne.graham@virginia.edu>
2012-06-22 09:05:43 -0400

    basic theme for uva_theme taking stuff from main site layout

--- common/footer.php
+++ common/footer.php
@@ -1,10 +1,29 @@
 
+</div>
+	</div><!-- end primary-wrap -->
+</div><!-- end content -->
 
+</div><!--end wrap-->
 
+<hr />
+<div id="globalFt">
+  <div class="docWrap">
+    <div class="col first">
 
-<?php plugin_footer(); ?>
+      <p><strong><a href="http://lib.virginia.edu">University&nbsp;of&nbsp;Virginia&nbsp;Library</a></strong><br />
+        PO Box 400113, Charlottesville, VA 22904-4113<br />
+        ph: (434) 924-3021, fax: (434) 924-1431, <a href="mailto:library@virginia.edu">library@virginia.edu</a></p>
+    </div>
+    <div class="col">
 
-</div> <!-- #container -->
+      <p><a href="http://www.lib.virginia.edu/libraries/">Libraries</a>&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;<a href="http://www.lib.virginia.edu/dir/libdept.html">Depts./Contacts</a>&nbsp;&nbsp; |&nbsp;&nbsp;<a href="http://www.virginia.edu">U.Va. Home</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.itc.virginia.edu">ITC</a></p>
+      <p><a href="mailto:webmanager_library@virginia.edu">Website Feedback</a>&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;<a href="http://www.lib.virginia.edu/sitesearch.html">Search</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.lib.virginia.edu/questions.html">Questions? Ask a Librarian</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.lib.virginia.edu/hours/summer_session_2007.html">Hours</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.lib.virginia.edu/map.html">Map</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.lib.virginia.edu/policies/">Policies</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.lib.virginia.edu/press/">Press</a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="http://www.lib.virginia.edu/HR/">Jobs</a></p>
+
+      <p><a href="http://www.virginia.edu/copyright.html" id="copyrightLink">&copy;</a> 2007 by the Rector and Visitors of the <a href="http://www.virginia.edu">University&nbsp;of&nbsp;Virginia</a></p>
+    </div>
+  </div>
+  <!-- End .docWrap -->
+</div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
--- common/header.php
+++ common/header.php
@@ -6,8 +6,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo get_html_lang(); ?>"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title>
-   <?php echo settings('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?>
+  <title><?php echo settings('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?>
   </title>
   <meta name="viewport" content="width=device-width">
   <?php if ($description = settings('description')): ?>
@@ -16,33 +15,39 @@
   <meta name="author" content="<?php echo settings('author'); ?>">
   <link rel="shortcut icon" href="<?php echo src('favicon.ico', 'images'); ?>">
   <link rel="apple-touch-icon" href="<?php echo src('apple-touch-icon.png', 'images'); ?>">
-
-  <?php echo js('libs/modernizr-2.5.3.min', 'javascripts/'); ?>
+  <link type="text/css" rel="stylesheet" href="http://yui.yahooapis.com/2.5.1/build/datatable/assets/skins/sam/datatable.css">
+  <?php echo js('libs/modernizr-2.5.3.min', 'javascripts'); ?>
 
   <?php echo auto_discovery_link_tags(); ?>
   <?php echo plugin_header(); ?>
   <?php
     queue_css('style');
     display_css();
-  ?>
+?>
+
 </head>
-<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
-<?php plugin_body(); ?>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => 'uvalib-t1 uvalib-hd1 yui-skin-sam ' . @$bodyclass)); ?>
 
-<div id="container">
+<div id="globalHd">
+  <div class="docWrap">
+    <div id="skipToNav" class="imgReplace"><strong>Skip directly to:</strong> <a href="#content">Main content</a> &nbsp;|&nbsp; <a href="#globalNav">Main navigation</a> &nbsp;|&nbsp; <a href="#sectNav">Navigation for this section of the website</a></div>
 
-<header role="banner" class="header">
-<?php plugin_page_header(); ?>
-</header>
+    <hr />
+    <div id="bookmarkMotif"> </div>
+    <div id="branding">
+      <span class="displayForNoCSS">Go to</span> <a id="logo" class="imgReplace" href="http://lib.virginia.edu" title="Go to U.Va. Library Home">University of Virginia Library</a> <span class="displayForNoCSS">home</span>    </div>
+    <hr />
+    <hr />
+  </div>
+  <!-- End .docWrap -->
+</div>
 
-<nav role="navigation">
-  <?php echo custom_public_nav_header(); ?>
-</nav>
+<hr />
 
-<div id="content" class="content">
-<?php plugin_page_content(); ?>
+<div id="content">
+  <div class="docWrap">
+    <div id="mainContent">
 
-
-
-
+    <div class="hd"><h1><?php echo settings('site_title'); ?></h1></div>
+      <div class="bd">
 
