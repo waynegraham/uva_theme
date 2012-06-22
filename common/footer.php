006ee1e30c7ccbebc41f08a7e3eda11a8562c89a
Wayne Graham <wayne.graham@virginia.edu>
2012-06-22 19:28:00 -0400

    removes old images; updates the main theme

--- common/footer.php
+++ common/footer.php
@@ -35,6 +35,9 @@
 <script>
 var _paq=_paq||[];(function(){var a="https:"==document.location.protocol?"https://analytics.lib.virginia.edu/":"http://analytics.lib.virginia.edu/";_paq.push(["setSiteId","10"]);_paq.push(["setTrackerUrl",a+"piwik.php"]);_paq.push(["trackPageView"]);_paq.push(["enableLinkTracking"]);var b=document,c=b.createElement("script"),d=b.getElementsByTagName("script")[0];c.type="text/javascript";c.defer=true;c.async=true;c.src=a+"piwik.js";d.parentNode.insertBefore(c,d)})()
 </script>
+
+<script type="text/javascript" src="http://use.typekit.com/txp7zqw.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 <!--[if lt IE 7 ]>
   <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
--- common/header.php
+++ common/header.php
@@ -15,7 +15,6 @@
   <meta name="author" content="<?php echo settings('author'); ?>">
   <link rel="shortcut icon" href="<?php echo src('favicon.ico', 'images'); ?>">
   <link rel="apple-touch-icon" href="<?php echo src('apple-touch-icon.png', 'images'); ?>">
-  <link type="text/css" rel="stylesheet" href="http://yui.yahooapis.com/2.5.1/build/datatable/assets/skins/sam/datatable.css">
   <?php echo js('libs/modernizr-2.5.3.min', 'javascripts'); ?>
 
   <?php echo auto_discovery_link_tags(); ?>
@@ -25,24 +24,51 @@
     display_css();
 ?>
 
+  <!--[if lt IE 8]><?php echo css('ie'); ?><![endif]-->
+
 </head>
-<?php echo body_tag(array('id' => @$bodyid, 'class' => 'uvalib-t1 uvalib-hd1 yui-skin-sam ' . @$bodyclass)); ?>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => 'home blog logged-in admin-bar  no-bg ' . @$bodyclass)); ?>
 
-<div id="globalHd">
-  <div class="docWrap">
-    <div id="skipToNav" class="imgReplace"><strong>Skip directly to:</strong> <a href="#content">Main content</a> &nbsp;|&nbsp; <a href="#globalNav">Main navigation</a> &nbsp;|&nbsp; <a href="#sectNav">Navigation for this section of the website</a></div>
+<div id="wrap" role="document">
+    <div id="jump-menu" class="visuallyhidden">
+      Jump to: 
+      <ul>
+        <li><a href="#content">Main content</a></li>
+        <li><a href="#main-nav">Main navigation</a></li>
+        <li><a href="#search">Search</a></li>
+        <li><a href="http://lib.virginia.edu/services/accessibility/">Accessibility Services</a></li>
+      </ul>
+    </div>
 
-    <hr />
-    <div id="bookmarkMotif"> </div>
-    <div id="branding">
-      <span class="displayForNoCSS">Go to</span> <a id="logo" class="imgReplace" href="http://lib.virginia.edu" title="Go to U.Va. Library Home">University of Virginia Library</a> <span class="displayForNoCSS">home</span>    </div>
-    <hr />
-    <hr />
-  </div>
-  <!-- End .docWrap -->
-</div>
-
-<hr />
+     <div class="container top-container">
+      <div class="row top-row">
+        <nav class="sixcol" id="top-nav">
+          <h3 class="visuallyhidden">U.Va. Links</h3>
+          <ul>
+            <li><a class="return-home" href="http://localhost:8888/wordpress/">Test Home</a></li>
+            <li><a href="http://www.virginia.edu">U.Va. Home</a></li>
+            <li><a href="http://lib.virginia.edu">U.Va. Library</a></li>
+          </ul>
+        </nav>
+        <div class="sixcol last">
+                  </div>
+      </div>
+    </div>
+<div class="container header-container"><div class="row header-row clearfix">
+    <header id="top-banner" class="twelvecol" role="banner">
+            <div class="container">
+        <h1 class="ninecol" id="main-title">
+          <a href="http://localhost:8888/wordpress/">
+            <span class="donor-title">Just another WordPress site</span>
+            <span class="library-title">Test</span>        
+        </a></h1>
+        <a class="threecol last" id="logo" href="http://lib.virginia.edu">
+          <?php echo img('liblogo.pn'); ?>
+          <img src="/wordpress/wp-content/themes/libweb-base/img/liblogo.png" width="198" height="90" alt="Test">
+        </a>
+      </div>
+    </header>
+  </div></div><!-- /.row /.container -->
 
 <div id="content">
   <div class="docWrap">
