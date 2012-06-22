3ac735d8c16dd64dc1ac3581641b5f8b297e09a8
Wayne Graham <wayne.graham@virginia.edu>
2012-06-22 14:14:44 -0400

    Adds analytics token

--- common/footer.php
+++ common/footer.php
@@ -32,6 +32,9 @@
     queue_js('uva-theme-1.0-min', 'javascripts');
     echo display_js(false);
 ?>
+<script>
+var _paq=_paq||[];(function(){var a="https:"==document.location.protocol?"https://analytics.lib.virginia.edu/":"http://analytics.lib.virginia.edu/";_paq.push(["setSiteId","10"]);_paq.push(["setTrackerUrl",a+"piwik.php"]);_paq.push(["trackPageView"]);_paq.push(["enableLinkTracking"]);var b=document,c=b.createElement("script"),d=b.getElementsByTagName("script")[0];c.type="text/javascript";c.defer=true;c.async=true;c.src=a+"piwik.js";d.parentNode.insertBefore(c,d)})()
+</script>
 <!--[if lt IE 7 ]>
   <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
