0dca6efbc68925674890713e04912daac46c0939
Wayne Graham <wayne.graham@virginia.edu>
2012-06-23 21:53:12 -0400

    make humans.txt an href not a content type

--- common/header.php
+++ common/header.php
@@ -1,5 +1,4 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo get_html_lang(); ?>"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="<?php echo get_html_lang(); ?>"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="<?php echo get_html_lang(); ?>"> <![endif]-->
@@ -13,7 +12,7 @@
   <meta name="description" content="<?php echo $description; ?>">
   <?php endif; ?>
   <meta name="author" content="<?php echo settings('author'); ?>">
-  <link rel="author" content="<?php echo src('humans.txt'); ?>">
+  <link rel="author" href="<?php echo src('humans.txt'); ?>">
   <link rel="shortcut icon" href="<?php echo src('favicon.ico', 'images'); ?>">
   <link rel="apple-touch-icon" href="<?php echo src('apple-touch-icon.png', 'images'); ?>">
   <?php echo js('libs/modernizr-2.5.3.min', 'javascripts'); ?>
