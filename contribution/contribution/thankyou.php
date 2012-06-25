85eb2fe3c97746b9d6c02a6d921f25a7fd449b0f
Wayne Graham <wayne.graham@virginia.edu>
2012-06-25 14:16:55 -0400

    adds a thankyou and a custom type-form

---
+++ contribution/contribution/thankyou.php
@@ -0,0 +1,9 @@
+<?php head(); ?>
+<div id="primary">
+	<h1>Thank You for Contributing!</h1>
+  <p>Your contribution will show up in the archive once an administrator approves it. 
+  Meanwhile, feel free to <?php echo contribution_link_to_contribute('make another contribution'); ?>.
+<!-- or <a href="<?php echo uri('items/browse'); ?>">browse the archive</a>.-->
+</p>
+</div>
+<?php foot(); ?>
