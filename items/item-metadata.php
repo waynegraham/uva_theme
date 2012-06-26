ae998691a4a74ae0475abe4ac875fbea7c5db257
Wayne Graham <wayne.graham@virginia.edu>
2012-06-26 10:55:38 -0400

    fixes bad paragraph tag

--- items/item-metadata.php
+++ items/item-metadata.php
@@ -1,4 +1,2 @@
-<div class="element-set threecol">
-    <h2>Description</h2>
-    </p><?php echo html_escape(item('Dublin Core', 'Description')); ?></p>
-</div><!-- end element-set -->
+<h2>Description</h2>
+<p><?php echo html_escape(item('Dublin Core', 'Description')); ?></p>
