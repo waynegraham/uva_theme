6296568f99ca19de34a28263b8454ba007f33b02
Wayne Graham <wayne.graham@virginia.edu>
2012-06-26 13:30:34 -0400

    removes Paul Irish column heights plugin which didn't work

--- javascripts/uva-theme-1.0.js
+++ javascripts/uva-theme-1.0.js
@@ -763,23 +763,9 @@ jQuery(function($) {
       }
     });
 
-    // height functions
-    // usage: $(‘div.unevenheights’).setAllToMaxHeight()
-    $.fn.setAllToMaxHeight = function() {
-      return this.height(
-       Math.max.apply(
-         this,
-         $.map(
-            this,
-            function(e) {
-              return $(e).height();
-            }
-         )
-      )
-      );
-    }
+    
 
-    $('#content').setAllToMaxHeight();
+
 
 });
 
