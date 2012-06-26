7abbfe5ee01060dd12a1d5f2b023467ee63b690e
Wayne Graham <wayne.graham@virginia.edu>
2012-06-26 10:49:39 -0400

    fixes for the items show page layout; restricts item metadata to just the description

---
+++ items/item-metadata.php
@@ -0,0 +1,4 @@
+<div class="element-set threecol">
+    <h2>Description</h2>
+    </p><?php echo html_escape(item('Dublin Core', 'Description')); ?></p>
+</div><!-- end element-set -->
---
+++ items/show.php
@@ -0,0 +1,44 @@
+<?php
+
+head(
+  array(
+    'title' => item(
+      'Dublin Core',
+      'Title'
+    ),
+    'bodyid'=>'items',
+    'bodyclass' => 'show'
+  )
+);
+?>
+
+<div id="primary">
+
+    <h1><?php echo item('Dublin Core', 'Title'); ?></h1>
+    <div  class="row clearfix">
+      <!-- The following returns all of the files associated with an item. -->
+      <div id="itemfiles" class="threecol">
+        <?php echo display_files_for_item(); ?>
+      </div>
+
+      <div class="element-set threecol">
+        <?php echo custom_show_item_metadata(); ?>
+      </div>
+    </div>
+
+    <!-- The following prints a citation for this item. -->
+    <div id="item-citation" class="element">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text"><?php echo item_citation(); ?></div>
+    </div>
+
+    <?php echo plugin_append_to_items_show(); ?>
+
+    <ul class="pager item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item(); ?></li>
+    </ul>
+
+</div><!-- end primary -->
+
+<?php foot(); ?>
